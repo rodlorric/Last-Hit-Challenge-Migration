d4d8333d4670646b2a61d7d4f84ac8fbe0713ed8
Ricardo Rodríguez Lorenzo <rrodriguez86@gmail.com>
2017-09-30 10:26:43 +0100

    Ignore python paths

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
-import os
-
-print(os.environ)
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+##!/usr/bin/python
+#import os
+#
+#print(os.environ)
+#virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+#virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+#try:
+#    execfile(virtualenv, dict(__file__=virtualenv))
+#except IOError:
+#    pass
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
